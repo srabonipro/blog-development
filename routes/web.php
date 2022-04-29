9196a7320c2d28e80afcd68de91b8d8deacccc2a
srabonipro <sraboni391@gmail.com>
2022-04-29 15:22:27 +0600

    starter commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth', 'is_admin'])->group(function(){
+    Route::resource('/products', ProductController::class);
+});
+
+require __DIR__.'/auth.php';
