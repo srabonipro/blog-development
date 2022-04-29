899376cc675c36ecd4d09f95c05d19ca7410fda8
srabonipro <sraboni391@gmail.com>
2022-04-29 15:48:46 +0600

    setup admin panel

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('backend.index');
 })->middleware(['auth'])->name('dashboard');
 
 Route::middleware(['auth', 'is_admin'])->group(function(){
