bf7f86ec7324dee3586ae487593e071bbdb3b7e2
srabonipro <sraboni391@gmail.com>
2022-05-14 11:31:43 +0600

    delete tag

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -90,8 +90,11 @@ class TagController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Tag $tag)
     {
-        //
+        $tag->delete();
+
+        toast('Tag Deleted Successfully!', 'success');
+        return back();
     }
 }
