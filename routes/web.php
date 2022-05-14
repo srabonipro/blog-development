7dd6e0014acf3c4cd2555281ec6453f1a3c2830e
srabonipro <sraboni391@gmail.com>
2022-05-14 11:09:33 +0600

    auto formated

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ Route::get('/admin/dashboard', function () {
     return view('backend.index');
 })->middleware(['auth'])->name('dashboard');
 
-Route::middleware(['auth', 'is_admin'])->prefix('admin')->group(function(){
+Route::middleware(['auth', 'is_admin'])->prefix('admin')->group(function () {
     Route::resource('/products', ProductController::class);
     Route::resource('/categories', CategoryController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
