a24c976cf6f90b0b6d73866434c0a75e4411b9e7
srabonipro <sraboni391@gmail.com>
2022-05-16 17:44:12 +0600

    add user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,5 +21,39 @@ class UserSeeder extends Seeder
             'password' => bcrypt('password'),
             'is_admin' => 1
         ]);
+
+
+        User::create([
+            'name' => 'Editor One',
+            'email' => 'editor@gmail.com',
+            'password' => bcrypt('password'),
+            'is_admin' => 0
+        ]);
+        User::create([
+            'name' => 'Editor Two',
+            'email' => 'editor2@gmail.com',
+            'password' => bcrypt('password'),
+            'is_admin' => 0
+        ]);
+
+
+        User::create([
+            'name' => 'Writer One',
+            'email' => 'writer@gmail.com',
+            'password' => bcrypt('password'),
+            'is_admin' => 0
+        ]);
+        User::create([
+            'name' => 'Writer Two',
+            'email' => 'writer2@gmail.com',
+            'password' => bcrypt('password'),
+            'is_admin' => 0
+        ]);
+        User::create([
+            'name' => 'Writer Three',
+            'email' => 'writer3@gmail.com',
+            'password' => bcrypt('password'),
+            'is_admin' => 0
+        ]);
     }
 }
