bf3cf4b2251c1df2da0dfc2b9f4019ffa169f786
srabonipro <sraboni391@gmail.com>
2022-05-17 15:32:37 +0600

    dynamic home page

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
     public function home()
     {
-        return view('frontend.index');
+        $posts = Post::latest()->get();
+        $recentPosts = Post::with('category:id,name', 'user:id,name')->paginate(9);
+
+        return view('frontend.index', compact('posts', 'recentPosts'));
     }
 }
