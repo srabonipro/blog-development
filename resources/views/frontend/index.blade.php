af12f0d6ec56bae02761c35fc2fc9cd45abbf98d
srabonipro <sraboni391@gmail.com>
2022-05-21 14:15:05 +0600

    website templating

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,291 +1,146 @@
- <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Mini Blog</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link href="../../css.css?family=Muli:300,400,700|Playfair+Display:400,700,900" rel="stylesheet">
-
-    <link rel="stylesheet" href="{{ asset('frontend') }}/fonts/icomoon/style.css">
-    <link rel="stylesheet" href="{{ asset('frontend') }}/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ asset('frontend') }}/css/magnific-popup.css">
-    <link rel="stylesheet" href="{{ asset('frontend') }}/css/jquery-ui.css">
-    <link rel="stylesheet" href="{{ asset('frontend') }}/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="{{ asset('frontend') }}/css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="{{ asset('frontend') }}/css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="{{ asset('frontend') }}/fonts/flaticon/font/flaticon.css">
-    <link rel="stylesheet" href="{{ asset('frontend') }}/css/aos.css">
-
-    <link rel="stylesheet" href="{{ asset('frontend') }}/css/style.css">
-  </head>
-  <body>
-
-  <div class="site-wrap">
-
-    <div class="site-mobile-menu">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
-        </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
-    </div>
-
-    <header class="site-navbar" role="banner">
-      <div class="container-fluid">
-        <div class="row align-items-center">
-
-          <div class="col-12 search-form-wrap js-search-form">
-            <form method="get" action="#">
-              <input type="text" id="s" class="form-control" placeholder="Search...">
-              <button class="search-btn" type="submit"><span class="icon-search"></span></button>
-            </form>
-          </div>
-
-          <div class="col-4 site-logo">
-            <a href="index.html" class="text-black h2 mb-0">Mini Blog</a>
-          </div>
-
-          <div class="col-8 text-right">
-            <nav class="site-navigation" role="navigation">
-              <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block mb-0">
-                <li><a href="category.html">Home</a></li>
-                <li><a href="category.html">Politics</a></li>
-                <li><a href="category.html">Tech</a></li>
-                <li><a href="category.html">Entertainment</a></li>
-                <li><a href="category.html">Travel</a></li>
-                <li><a href="category.html">Sports</a></li>
-                <li class="d-none d-lg-inline-block"><a href="#" class="js-search-toggle"><span class="icon-search"></span></a></li>
-              </ul>
-            </nav>
-            <a href="#" class="site-menu-toggle js-menu-toggle text-black d-inline-block d-lg-none"><span class="icon-menu h3"></span></a></div>
-          </div>
-
-      </div>
-    </header>
-
-
+<x-website-layout>
     <div class="site-section bg-light">
-      <div class="container">
-        <div class="row align-items-stretch retro-layout-2">
-          <div class="col-md-4">
+        <div class="container">
+          <div class="row align-items-stretch retro-layout-2">
+            <div class="col-md-4">
 
-            @foreach ($col1Posts as $post)
-            <a href="{{ route('frontend.post', $post->slug) }}" class="h-entry mb-30 v-height gradient" style="background-image: url('{{ $post->image }}');">
-              <div class="text">
-                <h2>{{  $post->title }}</h2>
-                <span class="date">{{  $post->published_at->format('M d, Y') }}</span>
-              </div>
-            </a>
-            @endforeach
-
-          </div>
-          <div class="col-md-4">
-
-            @foreach ($col2Posts as $post)
-            <a href="{{ route('frontend.post', $post->slug) }}" class="h-entry img-5 h-100 gradient" style="background-image: url('{{ $post->image }}');">
-              <div class="text">
-                <div class="post-categories mb-3">
-                  <span class="post-category bg-danger">{{  $post->category->name }}</span>
+              @foreach ($col1Posts as $post)
+              <a href="{{ route('frontend.post', $post->slug) }}" class="h-entry mb-30 v-height gradient" style="background-image: url('{{ $post->image }}');">
+                <div class="text">
+                  <h2>{{  $post->title }}</h2>
+                  <span class="date">{{  $post->published_at->format('M d, Y') }}</span>
                 </div>
-                <h2>{{  $post->title }}</h2>
-                <span class="date">{{  $post->published_at->format('M d, Y') }}</span>
-              </div>
-            </a>
-            @endforeach
+              </a>
+              @endforeach
 
-          </div>
-          <div class="col-md-4">
+            </div>
+            <div class="col-md-4">
 
-            @foreach ($col3Posts as $post)
-            <a href="{{ route('frontend.post', $post->slug) }}" class="h-entry mb-30 v-height gradient" style="background-image: url('{{ $post->image }}');">
-              <div class="text">
-                <h2>{{ $post->title }}</h2>
-                <span class="date">{{ $post->published_at->format('M d, Y') }}</span>
-              </div>
-            </a>
-            @endforeach
+              @foreach ($col2Posts as $post)
+              <a href="{{ route('frontend.post', $post->slug) }}" class="h-entry img-5 h-100 gradient" style="background-image: url('{{ $post->image }}');">
+                <div class="text">
+                  <div class="post-categories mb-3">
+                    <span class="post-category bg-danger">{{  $post->category->name }}</span>
+                  </div>
+                  <h2>{{  $post->title }}</h2>
+                  <span class="date">{{  $post->published_at->format('M d, Y') }}</span>
+                </div>
+              </a>
+              @endforeach
 
+            </div>
+            <div class="col-md-4">
+
+              @foreach ($col3Posts as $post)
+              <a href="{{ route('frontend.post', $post->slug) }}" class="h-entry mb-30 v-height gradient" style="background-image: url('{{ $post->image }}');">
+                <div class="text">
+                  <h2>{{ $post->title }}</h2>
+                  <span class="date">{{ $post->published_at->format('M d, Y') }}</span>
+                </div>
+              </a>
+              @endforeach
+
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="site-section">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-12">
-            <h2>Recent Posts</h2>
+      <div class="site-section">
+        <div class="container">
+          <div class="row mb-5">
+            <div class="col-12">
+              <h2>Recent Posts</h2>
+            </div>
           </div>
-        </div>
-        <div class="row">
+          <div class="row">
 
-            @foreach ($recentPosts as $post)
-                <div class="col-lg-4 mb-4">
-                    <div class="entry2">
-                        <a href="{{ route('frontend.post', $post->slug) }}"><img src="{{ $post->image }}" alt="Image" class="img-fluid rounded"></a>
-                        <div class="excerpt">
-                        <span class="post-category text-white bg-secondary mb-3">{{ $post->category->name }}</span>
+              @foreach ($recentPosts as $post)
+                  <div class="col-lg-4 mb-4">
+                      <div class="entry2">
+                          <a href="{{ route('frontend.post', $post->slug) }}"><img src="{{ $post->image }}" alt="Image" class="img-fluid rounded"></a>
+                          <div class="excerpt">
+                          <span class="post-category text-white bg-secondary mb-3">{{ $post->category->name }}</span>
 
-                        <h2><a href="{{ route('frontend.post', $post->slug) }}">{{ $post->title }}</a></h2>
-                        <div class="post-meta align-items-center text-left clearfix">
-                        <figure class="author-figure mb-0 mr-3 float-left"><img src="{{ $post->user->image }}" alt="Image" class="img-fluid"></figure>
-                        <span class="d-inline-block mt-1">By <a href="#">{{ $post->user->name }}</a></span>
-                        <span>&nbsp;-&nbsp; {{ $post->published_at->format('M d, Y') }}</span>
-                        </div>
-                        <p>
-                            {!! Str::limit(strip_tags($post->description), 100) !!}
-                        </p>
-                        <p><a href="{{ route('frontend.post', $post->slug) }}">Read More</a></p>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+                          <h2><a href="{{ route('frontend.post', $post->slug) }}">{{ $post->title }}</a></h2>
+                          <div class="post-meta align-items-center text-left clearfix">
+                          <figure class="author-figure mb-0 mr-3 float-left"><img src="{{ $post->user->image }}" alt="Image" class="img-fluid"></figure>
+                          <span class="d-inline-block mt-1">By <a href="#">{{ $post->user->name }}</a></span>
+                          <span>&nbsp;-&nbsp; {{ $post->published_at->format('M d, Y') }}</span>
+                          </div>
+                          <p>
+                              {!! Str::limit(strip_tags($post->description), 100) !!}
+                          </p>
+                          <p><a href="{{ route('frontend.post', $post->slug) }}">Read More</a></p>
+                          </div>
+                      </div>
+                  </div>
+              @endforeach
 
-        </div>
-        <div class="row text-center pt-5 border-top">
-          <div class="col-md-12">
-              {{ $recentPosts->links() }}
+          </div>
+          <div class="row text-center pt-5 border-top">
+            <div class="col-md-12">
+                {{ $recentPosts->links() }}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="site-section bg-light">
-      <div class="container">
-        <div class="row align-items-stretch retro-layout">
-          <div class="col-md-5 order-md-2">
-            @foreach ($footerPost1 as $post)
-            <a href="{{ route('frontend.post', $post->slug) }}" class="hentry img-1 h-100 gradient" style="background-image: url('{{ $post->image }}');">
-              <span class="post-category text-white bg-danger">{{  $post->category->name }}</span>
-              <div class="text">
-                <h2>{{ $post->title }}</h2>
-                <span>{{ $post->published_at->format('M d, Y') }}</span>
-              </div>
-            </a>
-            @endforeach
-          </div>
-
-          <div class="col-md-7">
-            @foreach ($footerPost3 as $post)
-            <a href="{{ route('frontend.post', $post->slug) }}" class="hentry img-2 v-height mb30 gradient" style="background-image: url('{{ $post->image }}');">
-              <span class="post-category text-white bg-success">{{  $post->category->name }}</span>
-              <div class="text text-sm">
-                <h2>{{ $post->title }}</h2>
-                <span>{{ $post->published_at->format('M d, Y') }}</span>
-              </div>
-            </a>
-            @endforeach
-            <div class="two-col d-block d-md-flex justify-content-between">
-                @foreach ($footerPost2 as $post)
-                <a href="{{ route('frontend.post', $post->slug) }}" class="hentry v-height img-2 gradient" style="background-image: url('{{ $post->image }}');">
-                    <span class="post-category text-white bg-primary">{{  $post->category->name }}</span>
-                    <div class="text text-sm">
-                    <h2>{{ $post->title }}</h2>
-                    <span>{{ $post->published_at->format('M d, Y') }}</span>
-                    </div>
-                </a>
+      <div class="site-section bg-light">
+        <div class="container">
+          <div class="row align-items-stretch retro-layout">
+            <div class="col-md-5 order-md-2">
+              @foreach ($footerPost1 as $post)
+              <a href="{{ route('frontend.post', $post->slug) }}" class="hentry img-1 h-100 gradient" style="background-image: url('{{ $post->image }}');">
+                <span class="post-category text-white bg-danger">{{  $post->category->name }}</span>
+                <div class="text">
+                  <h2>{{ $post->title }}</h2>
+                  <span>{{ $post->published_at->format('M d, Y') }}</span>
+                </div>
+              </a>
               @endforeach
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
 
-
-    <div class="site-section bg-lightx">
-      <div class="container">
-        <div class="row justify-content-center text-center">
-          <div class="col-md-5">
-            <div class="subscribe-1 ">
-              <h2>Subscribe to our newsletter</h2>
-              <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit nesciunt error illum a explicabo, ipsam nostrum.</p>
-              <form action="#" class="d-flex">
-                <input type="text" class="form-control" placeholder="Enter your email address">
-                <input type="submit" class="btn btn-primary" value="Subscribe">
-              </form>
+            <div class="col-md-7">
+              @foreach ($footerPost3 as $post)
+              <a href="{{ route('frontend.post', $post->slug) }}" class="hentry img-2 v-height mb30 gradient" style="background-image: url('{{ $post->image }}');">
+                <span class="post-category text-white bg-success">{{  $post->category->name }}</span>
+                <div class="text text-sm">
+                  <h2>{{ $post->title }}</h2>
+                  <span>{{ $post->published_at->format('M d, Y') }}</span>
+                </div>
+              </a>
+              @endforeach
+              <div class="two-col d-block d-md-flex justify-content-between">
+                  @foreach ($footerPost2 as $post)
+                  <a href="{{ route('frontend.post', $post->slug) }}" class="hentry v-height img-2 gradient" style="background-image: url('{{ $post->image }}');">
+                      <span class="post-category text-white bg-primary">{{  $post->category->name }}</span>
+                      <div class="text text-sm">
+                      <h2>{{ $post->title }}</h2>
+                      <span>{{ $post->published_at->format('M d, Y') }}</span>
+                      </div>
+                  </a>
+                @endforeach
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
 
 
-    <div class="site-footer">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-md-4">
-            <h3 class="footer-heading mb-4">About Us</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat reprehenderit magnam deleniti quasi saepe, consequatur atque sequi delectus dolore veritatis obcaecati quae, repellat eveniet omnis, voluptatem in. Soluta, eligendi, architecto.</p>
-          </div>
-          <div class="col-md-3 ml-auto">
-            <!-- <h3 class="footer-heading mb-4">Navigation</h3> -->
-            <ul class="list-unstyled float-left mr-5">
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Advertise</a></li>
-              <li><a href="#">Careers</a></li>
-              <li><a href="#">Subscribes</a></li>
-            </ul>
-            <ul class="list-unstyled float-left">
-              <li><a href="#">Travel</a></li>
-              <li><a href="#">Lifestyle</a></li>
-              <li><a href="#">Sports</a></li>
-              <li><a href="#">Nature</a></li>
-            </ul>
-          </div>
-          <div class="col-md-4">
-
-
-            <div>
-              <h3 class="footer-heading mb-4">Connect With Us</h3>
-              <p>
-                <a href="#"><span class="icon-facebook pt-2 pr-2 pb-2 pl-0"></span></a>
-                <a href="#"><span class="icon-twitter p-2"></span></a>
-                <a href="#"><span class="icon-instagram p-2"></span></a>
-                <a href="#"><span class="icon-rss p-2"></span></a>
-                <a href="#"><span class="icon-envelope p-2"></span></a>
-              </p>
+      <div class="site-section bg-lightx">
+        <div class="container">
+          <div class="row justify-content-center text-center">
+            <div class="col-md-5">
+              <div class="subscribe-1 ">
+                <h2>Subscribe to our newsletter</h2>
+                <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit nesciunt error illum a explicabo, ipsam nostrum.</p>
+                <form action="#" class="d-flex">
+                  <input type="text" class="form-control" placeholder="Enter your email address">
+                  <input type="submit" class="btn btn-primary" value="Subscribe">
+                </form>
+              </div>
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-12 text-center">
-            <p>
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              </p>
-          </div>
-        </div>
       </div>
-    </div>
-
-  </div>
-
-  <script src="{{ asset('frontend') }}/js/jquery-3.3.1.min.js"></script>
-  <script src="{{ asset('frontend') }}/js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="{{ asset('frontend') }}/js/jquery-ui.js"></script>
-  <script src="{{ asset('frontend') }}/js/popper.min.js"></script>
-  <script src="{{ asset('frontend') }}/js/bootstrap.min.js"></script>
-  <script src="{{ asset('frontend') }}/js/owl.carousel.min.js"></script>
-  <script src="{{ asset('frontend') }}/js/jquery.stellar.min.js"></script>
-  <script src="{{ asset('frontend') }}/js/jquery.countdown.min.js"></script>
-  <script src="{{ asset('frontend') }}/js/jquery.magnific-popup.min.js"></script>
-  <script src="{{ asset('frontend') }}/js/bootstrap-datepicker.min.js"></script>
-  <script src="{{ asset('frontend') }}/js/aos.js"></script>
-
-  <script src="js/main.js"></script>
-
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async="" src="../../gtag/js.js?id=UA-23581568-13"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-23581568-13');
-</script>
-
-  </body>
-</html>
+</x-website-layout>
